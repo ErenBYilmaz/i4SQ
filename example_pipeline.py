a8b752dbc43151c471e8f01a7581ad9cbfd5156a
Eren Yilmaz <eren.yilmaz@rad.uni-kiel.de>
2024-05-30 15:05:22 +0200

    Prepare an example pipeline

--- example_pipeline.py
+++ example_pipeline.py
@@ -1,9 +1,21 @@
+from lib.util import listdir_fullpath
+from model.ensemble.fnet_ensemble import FNetEnsembleBuilder
+from model.fnet.fnet import FNet
 from model.hnet_fnet.hnet_fnet import HNetFNetPipeline
+from model.pbl.pbl import PBL
 
 PBL_MODEL_PATH = r'./models/pbl/model_2020_01_23.pbl'
 FNET_MODELS_PATH = r'./models/fnet'
 
-def pipeline():
+
+def pipeline() -> HNetFNetPipeline:
+    some_fnet_model = [p for p in listdir_fullpath(FNET_MODELS_PATH) if p.endswith('.h5')][0]
     return HNetFNetPipeline(
-        hnet=Pbl()
+        hnet=PBL(
+            model_exe_path=PBL_MODEL_PATH,
+            model_version=8,
+        ),
+        fnet=FNet(
+            model_path=some_fnet_model,
+        )
     )
